2881938aa3728c6767cb12e132f0801438ad1038
alex <nalleballeuwu@gmail.com>
2025-01-16 11:10:59 +0100

    first commit

---
+++ src/main/java/com/example/receptboken/ReceptbokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.receptboken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptbokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptbokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/receptboken/ReceptbokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.receptboken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptbokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
